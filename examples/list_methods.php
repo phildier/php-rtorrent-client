de3f082b722e8096dbdbcc035d41d5b92adcd47c
blackout <blackout@drunkenlords.com>
2015-11-09 14:31:40 +1000

    Updated & Added New Examples

--- examples/list_methods.php
+++ examples/list_methods.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once("../vendor/autoload.php");
+require_once('../vendor/autoload.php');
 
-$client = new PHPRtorrentClient\Client(array('rpc_address'=>"http://localhost:8981/RPC2"));
+$client = new \PHPRtorrentClient\Client([
+	'rpc_address'	=>	'http://localhost:8981/RPC2',
+]);
 
-$request = new PHPRtorrentClient\Request("system.listMethods");
-
-$methods = $client->exec($request);
-
-print_r($methods->getAll());
+var_dump($client->listMethods());
